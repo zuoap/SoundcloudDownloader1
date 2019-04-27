2d014e2a308468c6553eff64dd7c5c5eb80139ca
Semyon Pyankov <semyon.pyankov@gmail.com>
2019-04-27 16:46:39 +0300

    пыпа

--- app/src/main/java/imta/soundclouddownloader/MainActivity.java
+++ app/src/main/java/imta/soundclouddownloader/MainActivity.java
@@ -2,12 +2,14 @@ package imta.soundclouddownloader;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        SharedPreferences GET = getSharedPreferences("path", 0);
     }
 
     @Override
@@ -40,9 +44,22 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                // Хуячь
+                Toast.makeText(this, "Хуй", Toast.LENGTH_SHORT).show();
+
+
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -52,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void inputUrlBtn(View view) {
         EditText url = findViewById(R.id.url_text);
-        SoundcloudDownloader.DOWNLOAD_DIR = ;
+//        SoundcloudDownloader.DOWNLOAD_DIR = ;
         SoundcloudDownloader.download(SoundcloudDownloader.getInfo(url.getText().toString()));
     }
 }
---
+++ app/src/main/java/imta/soundclouddownloader/Settings.java
@@ -0,0 +1,21 @@
+package imta.soundclouddownloader;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+//        SharedPreferences settings = getSharedPreferences("path", 0);
+//        SharedPreferences.Editor editor = settings.edit();
+//        editor.putString("path", );
+//        editor.apply();
+
+
+    }
+}
