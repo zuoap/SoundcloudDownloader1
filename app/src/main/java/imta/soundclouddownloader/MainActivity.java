ef89ea2f7e034253604b150d8e9957bbf0e84e1d
Semyon Pyankov <semyon.pyankov@gmail.com>
2019-04-27 16:50:17 +0300

    пыпа

--- app/src/main/java/imta/soundclouddownloader/MainActivity.java
+++ app/src/main/java/imta/soundclouddownloader/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
 //        SharedPreferences GET = getSharedPreferences("path", 0);
     }
 
